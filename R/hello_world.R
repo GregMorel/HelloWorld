39485ac66b68c13c7c1c3b80d59927a6472031e3
GregMorel <47211524+GregMorel@users.noreply.github.com>
2023-11-26 17:43:16 +0100

    Add files via upload

--- R/hello_world.R
+++ R/hello_world.R
@@ -1,3 +1,17 @@
+library(discretization)
+
 hello_world <- function(nom) {
   print(paste0("Hello, ", nom))
+}
+
+discret <- function() {
+  set.seed(123)
+  data <- data.frame(
+    attribute1 = rnorm(100),
+    attribute2 = runif(100),
+    class = sample(c("Class1", "Class2"), 100, replace = TRUE)
+  )
+  
+  discretized_data <- mdlp(data)
+  print(data$cutp[[1]])
 }
